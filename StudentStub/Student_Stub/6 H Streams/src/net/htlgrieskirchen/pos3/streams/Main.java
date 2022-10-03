8f7606d04b52a23b8fbfae2307f16cab242db320
timor <timo.reitboeck@gmx.at>
2022-10-03 17:34:34 +0200

    fill array with 100 random strings

--- StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Main.java
+++ StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Main.java
@@ -1,15 +1,12 @@
 package net.htlgrieskirchen.pos3.streams;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class Main {
 
 
     public static void main(String[] args) {
         Integer[] randoms = new Integer[10000];
         Streams s = new Streams();
-        randoms = s.fillArray();
+        randoms = s.fillIntArray();
         int[] values = new int[10000];
         for (int i = 0; i < randoms.length; i++) {
             values[i] = randoms[i];
--- StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
+++ StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
@@ -2,15 +2,16 @@ package net.htlgrieskirchen.pos3.streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalDouble;
+import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
 
-    Integer[] randoms = new Integer[10000];
-    public Integer[] fillArray()
+    Integer[] randomInts = new Integer[10000];
+    String[] randomStrings = new String[10];
+    public Integer[] fillIntArray()
     {
         Supplier<Integer> supplier = new Supplier<Integer>() {
             @Override
@@ -18,8 +19,24 @@ public class Streams {
                 return (int) (Math.random() *100);
             }
         };
-        randoms = Stream.generate(supplier).limit(10000).toArray(Integer[]::new);
-        return randoms;
+        randomInts = Stream.generate(supplier).limit(10000).toArray(Integer[]::new);
+        return randomInts;
+    }
+
+    public String[] fillStringArray()
+    {
+        Supplier<String> supplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                Random random = new Random();
+                return random.ints(97, 122 + 1)
+                        .limit(13)
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                        .toString();
+            }
+        };
+        randomStrings = Stream.generate(supplier).limit(100).toArray(String[]::new);
+        return randomStrings;
     }
 
     public double average(int[] numbers) {
@@ -28,7 +45,7 @@ public class Streams {
     }
 
     public List<String> upperCase(String[] strings) {
-        //implement this
+        
     }
 
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
