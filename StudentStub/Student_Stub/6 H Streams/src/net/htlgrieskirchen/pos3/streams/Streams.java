b8e68fbacbdfcfe9acd7966f03676d50824c4d7b
timor <timo.reitboeck@gmx.at>
2022-10-03 18:56:12 +0200

    99.9% of test done

--- StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
+++ StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
@@ -1,10 +1,10 @@
 package net.htlgrieskirchen.pos3.streams;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -12,20 +12,19 @@ public class Streams {
 
     Integer[] randomInts = new Integer[10000];
     String[] randomStrings = new String[10];
-    public Integer[] fillIntArray()
-    {
+
+    public Integer[] fillIntArray() {
         Supplier<Integer> supplier = new Supplier<Integer>() {
             @Override
             public Integer get() {
-                return (int) (Math.random() *100);
+                return (int) (Math.random() * 100);
             }
         };
         randomInts = Stream.generate(supplier).limit(10000).toArray(Integer[]::new);
         return randomInts;
     }
 
-    public String[] fillStringArray()
-    {
+    public String[] fillStringArray() {
         Supplier<String> supplier = new Supplier<String>() {
             @Override
             public String get() {
@@ -61,32 +60,48 @@ public class Streams {
     }
 
     public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
-        Weapon weaponStream = weapons.stream().sorted((w1, w2) -> Integer.compare(w2.getMinStrength(), w1.getMinStrength())).findFirst().orElse(null);
+        List<Weapon> weaponStream = weapons.stream().filter(weapon -> {
+            if (weapon.getDamageType().equals(DamageType.MISSILE)) {
+                return true;
+            } else {
+                return false;
+            }
+        }).toList();
         return weaponStream;
     }
 
     public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
-        //implement this
+        Weapon weaponStream = weapons.stream().sorted((w1, w2) -> Integer.compare(w2.getName().length(), w1.getName().length())).findFirst().orElse(null);
+        return weaponStream;
     }
 
     public List<String> toNameList(List<Weapon> weapons) {
-        //implement this
+        List<String> weaponStream = weapons.stream().map(Weapon::getName).toList();
+        return weaponStream;
     }
 
     public int[] toSpeedArray(List<Weapon> weapons) {
-        //implement this
+        List<Integer> weaponStream = weapons.stream().map(Weapon::getSpeed).toList();
+        int[] returnArray = new int[weaponStream.size()];
+        for (int i = 0; i < weaponStream.size(); i++) {
+            returnArray[i] = weaponStream.get(i);
+        }
+        return returnArray;
     }
 
     public int sumUpValues(List<Weapon> weapons) {
-        //implement this
+        int value = weapons.stream().mapToInt(Weapon::getValue).sum();
+        return value;
     }
 
     public long sumUpHashCodes(List<Weapon> weapons) {
-        //implement this
+        int value = weapons.stream().mapToInt(Weapon::hashCode).sum();
+        return value;
     }
 
     public List<Weapon> removeDuplicates(List<Weapon> weapons) {
-        //implement this
+        List<Weapon> weaponStream = weapons.stream().distinct().collect(Collectors.toList());
+        return weaponStream;
     }
 
     public void increaseValuesByTenPercent(List<Weapon> weapons) {
