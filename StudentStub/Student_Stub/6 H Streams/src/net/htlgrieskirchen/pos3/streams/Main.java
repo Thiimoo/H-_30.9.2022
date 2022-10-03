3816af48a4a58799d911781279bfbaaca6e76e57
timor <timo.reitboeck@gmx.at>
2022-10-03 16:49:21 +0200

    fill array with 10000 random numbers

---
+++ StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Main.java
@@ -0,0 +1,12 @@
+package net.htlgrieskirchen.pos3.streams;
+
+import java.util.stream.Stream;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Streams s = new Streams();
+        s.fillArray();
+    }
+}
--- StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
+++ StudentStub/Student_Stub/6 H Streams/src/net/htlgrieskirchen/pos3/streams/Streams.java
@@ -2,55 +2,72 @@ package net.htlgrieskirchen.pos3.streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Streams {
 
-    public double average(int[] numbers) {
-        //implement this
+    Integer[] randoms = new Integer[10000];
+    public void fillArray()
+    {
+        Supplier<Integer> supplier = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return (int) (Math.random() *10000);
+            }
+        };
+        randoms = Stream.generate(supplier).limit(10000).toArray(Integer[]::new);
+        System.out.println("123");
     }
-    
+
+    public double average(int[] numbers) {
+        IntStream stream = Arrays.stream(numbers);
+        
+    }
+
     public List<String> upperCase(String[] strings) {
         //implement this
     }
-    
+
     public Weapon findWeaponWithLowestDamage(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public Weapon findWeaponWithHighestStrength(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public List<Weapon> collectMissileWeapons(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public Weapon findWeaponWithLongestName(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public List<String> toNameList(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public int[] toSpeedArray(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public int sumUpValues(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public long sumUpHashCodes(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public List<Weapon> removeDuplicates(List<Weapon> weapons) {
         //implement this
     }
-    
+
     public void increaseValuesByTenPercent(List<Weapon> weapons) {
-       //implement this
+        //implement this
     }
+
 }
